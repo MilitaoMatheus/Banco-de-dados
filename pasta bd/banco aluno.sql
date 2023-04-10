5dca2d1a56a2cf7d0f666f32823e1cb0436a59c1
Matheus Militão da Silva <127455174+MilitaoMatheus@users.noreply.github.com>
2023-04-10 12:21:03 -0300

    Pasta banco de dados

---
+++ pasta bd/banco aluno.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE dbkevinmilito;
+
+USE dbkevinmilito;
+
+CREATE TABLE tbaluno(
+AlunoId int not null,
+Nome varchar(50) null,
+Endereco varchar(50) null,
+Estado char(2) not null
+);
+
+show databases;
+
+DESCRIBE dbkevinmilito;
---
+++ pasta bd/banco1.sql
@@ -0,0 +1,9 @@
+-- AUla ddo dia 10/04/2023 --
+
+-- comentário de linha --
+
+/* coomentário 
+da 
+aula
+na escola 
+*/
